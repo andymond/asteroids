fa9660c520277b50302ba2163effb032a7c31f69
Andrew Dymond <andy.dymond@gmail.com>
2018-04-14 14:59:00 -0600

    Remove faraday call from most_dangerous day controller

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::User::FavoritesController < ApiBaseController
   def index
-    render json: NasaNeoService.new.get_favorites(current_user)
+    render json: NasaService.new.get_favorites(current_user)
   end
 
   def create
     current_user.favorites.create(neo_reference_id: params[:neo_reference_id])
-    render json: NasaNeoService.new.get_asteroid(current_user, params[:neo_reference_id])
+    render json: NasaService.new.get_asteroid(current_user, params[:neo_reference_id])
   end
 end
--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,7 +1,7 @@
 class MostDangerousDayController < ApplicationController
   def index
-    response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=#{params[:start_date]}&end_date=#{params[:end_date]}&api_key=#{ENV['NASA_API_KEY']}")
-    results = JSON.parse(response.body, symbolize_names: true)
+    ns = NasaService.new
+    results = ns.get_asteroids_within_dates(params)
     parser = DangerousDayParser.new(results)
     @start_date = Time.parse(params[:start_date]).strftime("%B %-d, %Y")
     @end_date = Time.parse(params[:end_date]).strftime("%B %-d, %Y")

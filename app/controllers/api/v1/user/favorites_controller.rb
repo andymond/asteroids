4c938f87fa9d77a6d035514d6b2c9538646b8a6c
Andrew Dymond <andy.dymond@gmail.com>
2018-04-14 16:22:30 -0600

    refactor nasa_service to serve a single purpose & allow favorite serializer to be responsible for creating asteroids

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::User::FavoritesController < ApiBaseController
   def index
-    render json: NasaService.new.get_favorites(current_user)
+    render json: current_user.favorites, each_serializer: FavoriteSerializer
   end
 
   def create
-    current_user.favorites.create(neo_reference_id: params[:neo_reference_id])
-    render json: NasaService.new.get_asteroid(current_user, params[:neo_reference_id])
+    favorite = current_user.favorites.create(neo_reference_id: params[:neo_reference_id])
+    render json: favorite
   end
 end
--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -5,35 +5,11 @@ class NasaService
   end
 
   def get_asteroids_within_dates(dates)
-    asteroids = get_json("feed?start_date=#{dates[:start_date]}&end_date=#{dates[:end_date]}&api_key=#{ENV['NASA_API_KEY']}")
+    get_json("feed?start_date=#{dates[:start_date]}&end_date=#{dates[:end_date]}&api_key=#{ENV['NASA_API_KEY']}")
   end
 
-  def get_favorites(user)
-    favorites = user.favorites
-    favorites.map do |favorite|
-      attrs = get_json("neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
-      a = Asteroid.new(attrs)
-      { id: favorite.id,
-        neo_reference_id: favorite.neo_reference_id,
-        user_id: user.id,
-        asteroid: {name: a.name,
-          is_potentially_hazardous_asteroid: a.dangerous
-        }
-      }
-    end
-  end
-
-  def get_asteroid(user, id)
-    favorite = user.favorites.find_by(neo_reference_id: id)
-    attrs = get_json("neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
-    a = Asteroid.new(attrs)
-    { id: favorite.id,
-      neo_reference_id: favorite.neo_reference_id,
-      user_id: user.id,
-      asteroid: {name: a.name,
-        is_potentially_hazardous_asteroid: a.dangerous
-      }
-    }
+  def get_asteroid(neo_reference_id)
+    get_json("neo/#{neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
   end
 
   private

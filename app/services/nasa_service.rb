87a2dd29cc85869ac76c6e9fd0c9ef21ef067daf
Andrew Dymond <andy.dymond@gmail.com>
2018-04-14 14:58:19 -0600

    Rename nasa_neo_service to nasa servie & add get_asteroids_within_dates method

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -1,13 +1,17 @@
-class NasaNeoService
+class NasaService
 
   def initialize
-    @conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/neo/")
+    @conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/")
+  end
+
+  def get_asteroids_within_dates(dates)
+    asteroids = get_json("feed?start_date=#{dates[:start_date]}&end_date=#{dates[:end_date]}&api_key=#{ENV['NASA_API_KEY']}")
   end
 
   def get_favorites(user)
     favorites = user.favorites
     favorites.map do |favorite|
-      attrs = get_json("#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
+      attrs = get_json("neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
       a = Asteroid.new(attrs)
       { id: favorite.id,
         neo_reference_id: favorite.neo_reference_id,
@@ -21,7 +25,7 @@ class NasaNeoService
 
   def get_asteroid(user, id)
     favorite = user.favorites.find_by(neo_reference_id: id)
-    attrs = get_json("#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
+    attrs = get_json("neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
     a = Asteroid.new(attrs)
     { id: favorite.id,
       neo_reference_id: favorite.neo_reference_id,
